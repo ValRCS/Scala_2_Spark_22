9a8e33f0d9a2344cbe40a02c72e15c0a967e59b9
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-06-19 18:42:30 +0300

    Day 4 PartialFunction exercise

---
+++ src/main/scala/Day4CollectExercise.scala
@@ -0,0 +1,15 @@
+package com.github.ValRCS
+
+object Day4CollectExercise extends App {
+  println("Day 4 Exercise on Partial Functions and collect")
+  //TODO write two partial functions
+  //getEvenSquare applies to only positive even numbers  -> returns square
+  //getOddCube applies to only positive odd numbers -> returns cube
+  //combine both partial functions into a single partial function
+  //doPositives will work on positive numbers
+  val numbers = (-5 to 28).toArray
+  //using collect get the new values into
+  //TODO
+  //val processedNumbers =
+  //println the results
+}
