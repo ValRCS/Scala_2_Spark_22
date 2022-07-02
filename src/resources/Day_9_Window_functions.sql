a6b7e836a9bc805ef6408759f05f42422030f4bd
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-02 14:47:24 +0300

    Day 9 Window Functions Cumulative Sum

--- src/resources/Day_9_Window_functions.sql
+++ src/resources/Day_9_Window_functions.sql
@@ -135,3 +135,190 @@ FROM
 WHERE albumid IN (15,137)
 ORDER BY TrackId ;
 
+--https://www.sqlitetutorial.net/sqlite-window-functions/sqlite-dense_rank/
+
+SELECT
+	AlbumId,
+	Name,
+	Milliseconds,
+	DENSE_RANK () OVER ( 
+		PARTITION BY AlbumId 
+		--chances of having 2 songs with same millisecond length
+		--in same album are quite small
+		ORDER BY Milliseconds 
+	) LengthRank
+FROM
+	tracks;
+
+
+
+SELECT
+	AlbumId,
+	Name,
+	Milliseconds,
+	RANK () OVER ( 
+		PARTITION BY AlbumId 
+		--chances of having 2 songs with same millisecond length
+		--in same album are quite small
+		ORDER BY Milliseconds 
+	) LengthRank
+FROM
+	tracks;
+
+SELECT milliseconds FROM tracks t 
+GROUP BY Milliseconds 
+HAVING COUNT(Milliseconds) >=2;
+
+--https://www.sqlitetutorial.net/sqlite-window-functions/sqlite-percent_rank/
+--percent ranking 
+SELECT
+    Name,
+    Milliseconds,
+    PERCENT_RANK() OVER( 
+        ORDER BY Milliseconds 
+    ) LengthPercentRank,
+    --RANK and DENSE RANK will be same since no tiebreaks
+    RANK() OVER( 
+        ORDER BY Milliseconds 
+    ) LengthRank,
+     DENSE_RANK() OVER( 
+        ORDER BY Milliseconds 
+    ) LengthDenseRank
+FROM
+    tracks 
+WHERE
+    AlbumId = 1;
+   
+--LAG function lets us access some previous row within our window
+--https://www.sqlitetutorial.net/sqlite-window-functions/sqlite-lag/
+SELECT
+	CustomerId,
+	STRFTIME('%Y',InvoiceDate) Year,
+	--https://database.guide/how-to-extract-the-day-month-and-year-from-a-date-in-sqlite/
+	Total
+	FROM
+	Invoices 
+ORDER BY
+	CustomerId,
+	Year,
+	Total;
+
+
+SELECT
+	CustomerId,
+	STRFTIME('%Y',InvoiceDate) Year,
+	Total,
+	LAG ( Total, 1, 0 ) OVER ( 
+		ORDER BY InvoiceDate 
+	) PreviousYearTotal
+FROM
+	Invoices 
+WHERE
+	CustomerId = 4;
+
+--if we want to do it for all customers
+--then we will use partition by customerID
+SELECT
+	CustomerId,
+	STRFTIME('%Y',InvoiceDate) Year,
+	Total,
+	--in LAG(column, offsetback, default when nothing to go backto)
+	LAG ( Total,1,0) OVER ( 
+		PARTITION BY CustomerId --more precise than year
+		ORDER BY  InvoiceDate) PreviousYearTotal 
+FROM
+	Invoices;
+
+CREATE VIEW v_customer_totals_years
+AS
+SELECT
+	CustomerId,
+	STRFTIME('%Y',InvoiceDate) Year,
+	Total,
+	--in LAG(column, offsetback, default when nothing to go backto)
+	LAG ( Total,1,0) OVER ( 
+		PARTITION BY CustomerId --more precise than year
+		ORDER BY  InvoiceDate) PreviousYearTotal 
+FROM
+	Invoices;
+
+SELECT * FROM v_customer_totals_years;
+
+SELECT *, 
+total-previousyeartotal delta
+FROM v_customer_totals_years; 
+
+--similarly we can use LEAD to access next rows within window
+SELECT
+	CustomerId,
+	STRFTIME('%Y',InvoiceDate) Year,
+	InvoiceDate,
+	Total,
+	LAG ( Total, 1, 0 ) OVER (
+		PARTITION BY CustomerId 
+		ORDER BY InvoiceDate)
+		LastYearsTotal,
+	LEAD ( Total, 1, 0 ) OVER (
+		PARTITION BY CustomerId 
+		ORDER BY InvoiceDate
+	) NextYearTotal 
+FROM
+	Invoices;
+
+--now we can try to get a running total(cumulative sum)
+--
+SELECT 
+    CustomerID,
+    InvoiceDate,
+    Total,
+    SUM(total) OVER (
+        ORDER BY InvoiceDate
+    ) RunningTotal
+FROM
+   invoices i2 
+  WHERE CustomerId = 33;
+--https://www.sqlitetutorial.net/sqlite-window-functions/sqlite-window-frame/
+
+ --now we can create a running total for all customers
+ SELECT 
+    CustomerID,
+    InvoiceDate,
+    Total,
+    SUM(total) OVER (
+        PARTITION BY CustomerId --without partition it would be running total for ALL invoices
+        ORDER BY InvoiceDate
+    ) RunningTotal
+FROM
+   invoices i2 ;
+  
+--I can add running average as well
+ SELECT 
+    CustomerID,
+    InvoiceDate,
+    Total,
+    SUM(total) OVER (
+        PARTITION BY CustomerId --without partition it would be running total for ALL invoices
+        ORDER BY InvoiceDate
+    ) RunningTotal ,
+     AVG(total) OVER (
+        PARTITION BY CustomerId --without partition it would be running total for ALL invoices
+        ORDER BY InvoiceDate
+    ) RunningAverage ,
+    COUNT(total) OVER (
+    	PARTITION BY CustomerId
+    	ORDER BY InvoiceDate
+    ) RunningCount   
+FROM
+   invoices i2 ;
+
+  
+  --by default when we define OVER
+  --we get access to window of current row and 
+  --unbounded previous rows by our partitioning and ordering
+  --https://www.sqlite.org/windowfunctions.html
+  
+  --The default frame-spec is:
+
+--RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW EXCLUDE NO OTHERS
+--The default means that aggregate window functions read all rows from the beginning of the partition up to and including the current row and its peers
+
