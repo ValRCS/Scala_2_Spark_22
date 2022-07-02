ab0f5620af2a3a5c91a909a3c81e678922d4d9b9
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-02 11:11:59 +0300

    Day 9 Aggregation Functions exercises almost solved

--- src/resources/Day_9_Aggregation_Window_functions.sql
+++ src/resources/Day_9_Aggregation_Window_functions.sql
@@ -124,16 +124,175 @@ SELECT
 --TODO 1
 --Which city has the most invoices?
 --Order by invoice count
+SELECT BillingCity, COUNT(BillingCity) cnt FROM invoices i 
+GROUP BY BillingCity 
+ORDER BY cnt DESC;
 
 --TODO 2
 --Which cities has the best customers
 --This means we want to have an ordered list
 --5 best cities with highest sum of totals
+SELECT BillingCity, SUM(Total) total FROM invoices i 
+GROUP BY BillingCity 
+ORDER BY total DESC
+LIMIT 5;
+
+--we could combine the above queries in a single one
+--because we are using the same table and same group by
+SELECT BillingCity, 
+COUNT(BillingCity) orders, 
+SUM(Total) total, 
+ROUND(AVG(Total),2) averageOrder
+FROM invoices i 
+GROUP BY BillingCity 
+ORDER BY total DESC
+LIMIT 5;
 
 --TODO 3 Find the biggest 3 spenders
 --this might involve joining customers and invoices and invoice items
 --then using GROUP BY and then SUM on grouped TOTAL
+SELECT i.CustomerId, 
+		c.FirstName, 
+		c.LastName, 
+		c.Country,
+		c.City,
+		i.Total 
+--		SUM(total) totalByCustomer -- aggregate returns a single value
+FROM invoices i 
+JOIN customers c 
+	ON i.CustomerId = c.CustomerId ;
+
+
+SELECT i.CustomerId, 
+		c.FirstName, 
+		c.LastName, 
+		c.Country,
+		c.City,
+		SUM(total) totalByCustomer 
+FROM invoices i 
+JOIN customers c 
+	ON i.CustomerId = c.CustomerId 
+GROUP BY i.CustomerId 
+ORDER BY totalByCustomer DESC 
+LIMIT 3;
+
+--following query will give sum total multiple times
+--so not correct!
+--it happens because a single row represents a single item purchase
+SELECT c.CustomerId, FirstName, LastName, SUM(i.Total) total_spent_amount FROM customers c 
+JOIN invoices i
+ON i.CustomerId = c.CustomerId
+JOIN invoice_items ii 
+ON i.InvoiceId = ii.InvoiceId 
+GROUP BY i.CustomerId 
+ORDER BY total_spent_amount DESC
+LIMIT 3;
+
+SELECT c.CustomerId, 
+FirstName, 
+LastName, 
+i.Total,
+i.InvoiceId ,
+ii.UnitPrice,
+ii.Quantity 
+FROM customers c 
+JOIN invoices i
+ON i.CustomerId = c.CustomerId
+JOIN invoice_items ii 
+ON i.InvoiceId = ii.InvoiceId;
+
+SELECT * FROM customers c 
+WHERE city = 'Prague';
+
+--here the problem is that unit price is not the full picture
+SELECT c.CustomerID, 
+c.Firstname, 
+c.Lastname, 
+SUM(ii.UnitPrice) as sumUnits
+FROM invoices i 
+JOIN invoice_items ii ON ii.InvoiceId = i.InvoiceId 
+JOIN customers c ON c.CustomerId = i.InvoiceId 
+GROUP BY c.CustomerId
+ORDER by sumUnits DESC
+LIMIT 3;
+
+SELECT * FROM invoice_items ii
+ORDER BY UnitPrice DESC;
+
+SELECT * FROM invoice_items ii 
+JOIN invoices i 
+ON i.InvoiceId = ii.InvoiceId ;
+
+SELECT i.CustomerId, SUM(UnitPrice) sumPrices FROM invoice_items ii 
+JOIN invoices i 
+ON i.InvoiceId = ii.InvoiceId 
+JOIN customers c ON c.CustomerId = i.CustomerId 
+GROUP BY i.CustomerId 
+ORDER BY sumPrices DESC;
+
+
 
 --TODO 4 find ALL listeners to classical music
 -- include their names and emails and phone numbers
 --this might not need aggregation
+SELECT  c.FirstName, 
+		c.LastName, 
+		c.Email, 
+		c.Phone 
+FROM invoice_items ii  
+JOIN invoices i 
+	ON ii.InvoiceId = i.InvoiceId
+JOIN customers c 
+	ON i.CustomerId = c.CustomerId
+JOIN tracks t 
+	ON ii.TrackId = t.TrackId
+JOIN genres g 
+	ON t.GenreId = g.GenreId
+WHERE g.Name = 'Classical';
+
+--so we 
+SELECT DISTINCT (FirstName || LastName), FirstName, LastName
+FROM customers c 
+JOIN invoices i 
+ON c.CustomerId = i.CustomerId
+JOIN invoice_items ii 
+ON ii.InvoiceId = i.InvoiceId
+JOIN tracks t 
+ON t.TrackId = ii.TrackId 
+JOIN genres g 
+ON g.GenreId = t.GenreId
+WHERE g.Name = 'Classical'
+ORDER BY FirstName ;
+
+SELECT c.CustomerId, FirstName, LastName, Email, Phone, 
+COUNT(t.TrackId) classical_tracks_bought,
+SUM(t.Milliseconds) / 60000 minutes_bought
+FROM customers c 
+JOIN invoices i
+ON i.CustomerId = c.CustomerId
+JOIN invoice_items ii 
+ON i.InvoiceId = ii.InvoiceId 
+JOIN tracks t 
+ON t.TrackId = ii.TrackId 
+JOIN genres g 
+ON g.GenreId = t.GenreId 
+WHERE g.Name = 'Classical'
+GROUP BY i.CustomerId 
+ORDER BY classical_tracks_bought DESC;
+
+SELECT ii.TrackId, i.CustomerId, c.FirstName, c.LastName, c.Email, c.Phone, g.Name AS genreName
+FROM invoice_items ii  
+JOIN invoices i 
+	ON ii.InvoiceId = i.InvoiceId
+JOIN customers c 
+	ON i.CustomerId = c.CustomerId
+JOIN tracks t 
+	ON ii.TrackId = t.TrackId
+JOIN genres g 
+	ON t.GenreId = g.GenreId
+WHERE g.Name = 'Classical'
+GROUP BY i.CustomerId 
+ORDER BY i.CustomerId ;
+
+SELECT * FROM customers c2 
+WHERE c2.CustomerId BETWEEN 30 AND 40;
