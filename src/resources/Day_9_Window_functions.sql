32de98e213017e51e9424ea9df00ea9a16b9c02a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-02 12:03:34 +0300

    Day 9 Window Functions Rank

---
+++ src/resources/Day_9_Window_functions.sql
@@ -0,0 +1,5 @@
+--https://www.sqlite.org/windowfunctions.html
+--https://www.sqlitetutorial.net/sqlite-window-functions/
+
+--with window functions we do not group rows
+--we use function for each row using some criteria (usually surrounding rows)
