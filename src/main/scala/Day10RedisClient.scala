7783f45b16a3fd22919ae3420e8f58df536459b1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-04 18:13:45 +0300

    Day 10 Redis client connection setup

--- src/main/scala/Day10RedisClient.scala
+++ src/main/scala/Day10RedisClient.scala
@@ -9,8 +9,21 @@ object Day10RedisClient extends App {
   val url = "redis-19034.c10.us-east-1-4.ec2.cloud.redislabs.com"
   val dbName = "Scala2SparkCourse22"
 
-  val pw = Some("This should not be public") //best practice would be to load int from enviroment variable TODO show how
+//  val pw = Some("This should not be public") //best practice would be to load int from enviroment variable TODO show how
+  val pw = Some("do not put real password into git!!") //best practice would be to load int from enviroment variable TODO show how
 
   val r = new RedisClient(host=url, port, 0, secret= pw)
+
+  r.set("myname", "Valdis")
+  r.incr(key = "mycount") //so either initialie value to 1 or increment by 1
+  //we print values directly from database
+  println(r.get("myname"))
+  //or save into a value/variable
+  val myCounter = r.get("mycount")
+  println(s"My counter is at $myCounter")
+
+  //TODO connect to your own database not mine :)
+  //maybe test a few more commands incrBy or something else ?
+
 
 }
